33a22136fb91e1326b4301ce940921978d7afbbc
DanielGalvis-Dev <djgz2004@gmail.com>
2023-11-19 15:39:56 -0500

    react-prueba-tecnica

--- react-projects/04-prueba-tecnica/hooks/useCatImage.js
+++ react-projects/04-prueba-tecnica/hooks/useCatImage.js
@@ -8,14 +8,12 @@ function useCatImage({ fact }) {
     if (!fact) return;
 
     const threeFW = fact.split(" ", 3).join(" ");
-    console.log(threeFW);
     setThreeFirstWords(threeFW);
 
     fetch("https://api.thecatapi.com/v1/images/search")
       .then((res) => res.json())
       .then((response) => {
         const [{ url }] = response;
-        console.log(url);
         setImageURL(url);
       });
   }, [fact]);
